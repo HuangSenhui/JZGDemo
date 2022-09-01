024d9fbb7c0d56e9ce5e1a45328788abedc754d4
Senhui <huangsenhui@live.com>
2022-09-01 21:03:51 +0800

    Initial Commit

---
+++ JZGDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JZGDemo
+//
+//  Created by HuangSenhui on 2022/9/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
